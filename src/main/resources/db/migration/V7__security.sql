4fab92c3fc38006f053c37ba01fc5d8c838abee6
Rahul Kumar <rahularya0023@gmail.com>
2025-08-21 13:40:06 +0200

    deployment db updates

--- src/main/resources/db/migration/V7__security.sql
+++ src/main/resources/db/migration/V7__security.sql
@@ -1,7 +1,5 @@
 -- --- V2 security migration start ---
 UPDATE trips   SET owner = 'rahul0083.be@gmail.com' WHERE owner IS NULL OR owner = '' OR owner = 'anon';
 UPDATE budgets SET owner = 'rahul0083.be@gmail.com' WHERE owner IS NULL OR owner = '' OR owner = 'anon';
-UPDATE trips SET participants = array_append(COALESCE(participants, ARRAY[]::varchar[]), 'rahul0083.be@gmail.com')
-WHERE owner = 'rahul0083.be@gmail.com';
 
 -- --- V2 security migration end ---
